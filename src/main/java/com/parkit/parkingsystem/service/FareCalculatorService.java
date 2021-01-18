a836c4913e09c28e0fe68340017a0c21c2245645
yaunz <yaunz@DESKTOP-OPVE7IO.home>
2021-01-18 20:00:22 +0100

    Fix the code to make the unit tests pass

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,7 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.concurrent.TimeUnit;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -10,19 +12,20 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+      
+        long duration = outHour - inHour;
+        float durationInMinutes = TimeUnit.MINUTES.convert(duration, TimeUnit.MILLISECONDS);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(durationInMinutes * Fare.CAR_RATE_PER_HOUR/60);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(durationInMinutes * Fare.BIKE_RATE_PER_HOUR/60);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
